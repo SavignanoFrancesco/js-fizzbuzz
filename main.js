7f94e1e2f0ecf82df2fb51540b3637b06f08bb10
Francesco <francesco.savignano96@gmail.com>
2020-10-20 13:40:10 +0200

    repo impostata

---
+++ main.js
@@ -0,0 +1,4 @@
+//Scrivere un programma che stampi tutti i numeri da 1 a 100 con queste regole:
+//al posto dei multipli di 3, deve stampare "Fizz"
+//al posto dei multipli di 5, deve stampare "Buzz"
+//al posto dei multipli sia di 3 che di 5, deve stampare "FizzBuzz"
